341a65ee16226cc6652a98c7845d3b88f95a65b6
Joshua Pearson <joshuavillgren@gmail.com>
2024-04-19 11:39:37 +1200

    QUESTION ONE

--- data/creaturescripts/scripts/logout.lua
+++ data/creaturescripts/scripts/logout.lua
@@ -1,7 +1,16 @@
+local function releaseStorage(player)
+    player:setStorageValue(1000, -1)
+end
+
 function onLogout(player)
-	local playerId = player:getId()
-	if nextUseStaminaTime[playerId] then
+    if not player then
+        print("Player doesn't exist in this context")
+        return false
+    end
+    if nextUseStaminaTime[playerId] then
 		nextUseStaminaTime[playerId] = nil
-	end
-	return true
+    if player:getStorageValue(1000) == 1 then
+        releaseStorage(player)
+    end
+    return true
 end
