428e23802f0ea0661dd48e4324cc20cdcf3e085b
Joshua Pearson <joshuavillgren@gmail.com>
2024-04-19 11:42:01 +1200

    QUESTION THREE

--- data/lib/core/party.lua
+++ data/lib/core/party.lua
@@ -8,3 +8,31 @@ function Party.broadcastPartyLoot(self, text)
 		end
 	end
 end
+
+function do_sth_with_PlayerParty(playerId, membername)
+    local player = Player(playerId)
+    if not player then
+        print("Error: Player not found")
+        return
+    end
+
+    local party = player:getParty()
+    if not party then
+        print("Error: Player not in a party")
+        return
+    end
+
+    local memberToRemove = nil
+    for k,v in pairs(party:getMembers()) do
+        if v:getName() == membername then
+            memberToRemove = member
+            break
+        end
+    end
+
+    if memberToRemove then
+        party:removeMember(memberToRemove)
+    else
+        print("Error: Member not in party")
+    end
+end
